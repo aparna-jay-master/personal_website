cbdf2878d037ceb3e2a2632d996e243a54cb2695
Aparna <aparnajay.jay@gmail.com>
2017-10-25 18:01:46 -0700

    Move menu to the top

--- portfolio/js/helper.js
+++ portfolio/js/helper.js
@@ -2,9 +2,9 @@ var HTMLitemStart = '<div class="box"></div>';
 
 var HTMLtitle = '<h2>%data%</h2>';
 var HTMLgithub = '<h3 class="sub-color"><a target="_blank" href="%data%">GitHub</a></h3>';
-var HTMLimageUrl  = '<img class="portfolio-image" src="%data%"';
+var HTMLimageUrl  = '<div class=image-block><img class="portfolio-image" src="%data%"';
 var HTMLimageAlt = ' alt="%data%">';
-var HTMLdescription = '<p class="description">%data%</p>';
+var HTMLdescription = '<p class="description">%data%</p></div>';
 var HTMLskills = '<p class="italics sub-color">%data%</p>';
 
 var HTMLline = '<hr>';
